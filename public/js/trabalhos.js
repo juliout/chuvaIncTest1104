67f8d8004d7b5cfd2646d7d33e1e5accf3a63696
julio cesar <81329552+juliout@users.noreply.github.com>
2022-04-12 17:55:20 -0300

    Completed

--- public/js/trabalhos.js
+++ public/js/trabalhos.js
@@ -1,49 +1,23 @@
 // aumentar o resumo
-const textResume = document.querySelector('.textResume')
-const textOppened = document.getElementById('textOppened')
-const textClosed = document.getElementById('textClosed')
-let op = false
+
+const firstTextResume = document.getElementById('firstTextResume')
+const secondTextResume = document.getElementById('secondTextResume')
+let oppened = false
 
 function openDiv() {
 
-    if (op === false ) {
-        
-    textResume.innerHTML = `
-        <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus vitae turpis auctor, mollis felis ut, commodo turpis. Phasellus felis mauris, egestas eget cursus et, iaculis quis lacus. Fusce auctor eros sed magna ultricies gravida. Etiam aliquam dictum nisl, vel aliquet enim accumsan sit amet. Donec finibus nisi tellus, ut viverra lorem vestibulum ut. Phasellus condimentum orci id leo condimentum lobortis et non lorem. Sed id nisl metus. Quisque sollicitudin ligula in sapien scelerisque, ac gravida eros vestibulum.
-        </p>
-        <p> 
-            Fusce vitae luctus dui. Donec id euismod mauris, in volutpat urna. Proin dapibus consequat feugiat. Proin dictum justo arcu, quis vestibulum augue lacinia quis. Sed dignissim dui nulla, ut faucibus mauris sodales id. Aliquam erat volutpat. Maecenas dolor enim, tincidunt id elit non, suscipit interdum turpis. Etiam finibus urna libero, eget interdum eros volutpat ullamcorper. Pellentesque et pretium lorem. Aenean interdum quis diam ac porttitor. Cras nec ipsum pulvinar, pharetra libero non, ornare enim. Etiam in laoreet odio.
-        </p>
-        <p>
-            Nam eget tristique elit, at fermentum tellus. Mauris scelerisque ligula id eleifend feugiat. Donec eleifend vehicula sem nec dapibus. Integer scelerisque neque dui, in lacinia erat molestie eu. Phasellus maximus dui eget lacus porta tempor. Ut ex nibh, dignissim quis purus semper, efficitur facilisis turpis. Praesent blandit elementum ultricies. Aliquam sit amet enim sit amet nulla pulvinar lobortis consectetur non odio. Phasellus at lacus hendrerit, vulputate nisi sit amet, viverra mauris. Etiam eu scelerisque orci. Quisque sagittis, mi vitae pharetra iaculis, orci elit eleifend massa, eu posuere mauris odio id odio. Morbi eu libero luctus, consectetur lorem vel, interdum sapien. Aenean in porta arcu. Maecenas eu maximus massa.
-        </p>
-        <p>
-            Praesent velit dolor, dignissim sed quam ac, efficitur porta justo. Pellentesque porta pharetra felis ut hendrerit. Nulla facilisi. Aliquam erat volutpat. Nunc sit amet faucibus quam. Maecenas dapibus luctus dolor at viverra. Duis nec fringilla libero. Duis risus nibh, viverra ac orci nec, iaculis dictum sem. Aliquam at malesuada arcu. Aliquam erat volutpat. Donec varius ipsum purus, ut vehicula purus placerat vitae. Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-        </p>
-    `
-     op = true
-     return
-    } else {
-        textResume.innerHTML = `
-            <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus vitae turpis auctor, mollis felis ut, commodo turpis. Phasellus felis mauris, egestas eget cursus et, iaculis quis lacus. Fusce auctor eros sed magna ultricies gravida. Etiam aliquam dictum nisl, vel aliquet enim accumsan sit amet. Donec finibus nisi tellus, ut viverra lorem vestibulum ut.  Phasellus condimentum orci id leo condimentum lobortis et non lorem. Sed id nisl metus. Quisque sollicitudin ligula in sapien scelerisque, ac gravida eros vestibulum Phasellus condimentum orci id leo condimentum lobortis et non lorem. Sed id nisl metus. Quisque sollicitudin ligula in sapien scelerisque, ac gravida eros vestibulum.  Etiam aliquam dictum nisl, vel aliquet enim accumsan sit ametl accumsant... ver mais
-            </p>
-        `
-        op = false
+    if(!oppened) {
+        secondTextResume.classList.remove('divOff')
+        firstTextResume.classList.add('divOff')
+        oppened = true
         return
-    }
-
-    
+    } else {
+        secondTextResume.classList.add('divOff')
+        firstTextResume.classList.remove('divOff')
+        oppened = false
+        return
+    }    
 }
-
-
-
-
-
-
-
-
 //adicionar topico
 
 const containerDiscurssoes = document.querySelector('.containerDiscurssoes')
@@ -51,7 +25,6 @@ const containerSugestion = document.querySelector('.containerSugestion')
 const containerFinished = document.querySelector('.containerFinished')
 
 const comentsDiv = document.querySelector('.comentsDiv')
-
 
 function newTopic() {
     containerFinished.classList.add('divOff')
@@ -68,7 +41,7 @@ function sendTopic() {
     `
     <div class="comment">
             <div class="awaitAccept">
-                <img src="public/images/finishedIcon.png" alt="">
+                <img src="public/images/finishedIcon.png" alt="finishedicon">
                 <span>Aguardando feedback dos autores</span>
                 <a href="">Editar tópico</a>
             </div>
@@ -84,17 +57,15 @@ function sendTopic() {
                 <div class="likedDiv">
                     <img src="/public/images/dots.svg" alt="" class="dots">
                     <button>
-                        <img src="public/images/liked.png" alt="">
+                        <img src="public/images/liked.png" alt="liked">
                     </button>
                     <span>1 like</span>
                     <span>1 resposta </span>
                 </div>
             </div>
-            
         </div>
     `
     )
-    
 }
 
 //abre os comentarios
